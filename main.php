584f3ddb407dd3af47ee3f3da686bde518e0a179
Jaroslav Svetlik <77618893+majestic-code@users.noreply.github.com>
2023-03-14 15:56:38 +0100

    Create main.php

---
+++ main.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+  'beograd' => [
+    'barajevo' => 'Barajevo',
+    'vozdovac' => 'Voždovac',
+    'vracar' => 'Vračar',
+    'grocka' => 'Grocka',
+    'zvezdara' => 'Zvezdara',
+    'zemun' =>  'Zemun',
+    'lazarevac' => 'Lazarevac',
+    'mladenovac' => 'Mladenovac',
+    'novi-beograd' => 'Novi Beograd',
+    'obrenovac' => 'Obrenovac',
+    'palilula' => 'Palilula',
+    'rakovica' => 'Rakovica',
+    'savski-venac' => 'Savski Venac',
+    'sopot' => 'Sopot',
+    'stari-grad' => 'Stari Grad',
+    'surcin' => 'Surcin',
+    'cukarica' => 'Čukarica',
+  ]
+];
